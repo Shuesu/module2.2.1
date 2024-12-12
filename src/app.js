aa87f0d0d4367168225e8cd8c8cc1c55d9a91927
Atabayev Ali <sptmbrw@gmail.com>
2024-12-12 22:50:37 +0500

    add functional part

--- src/app.js
+++ src/app.js
@@ -1,28 +1,58 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './app.module.css';
 
 export const App = () => {
+	const [value, setValue] = useState('');
+	const [list, setList] = useState([]);
+	const [error, setError] = useState('');
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt('Введите значение');
+		if (promptValue.length >= 3) {
+			setValue(promptValue);
+			setError('');
+		} else {
+			setError('Введенное значение должно содержать минимум 3 символа');
+		}
+	};
+
+	const onAddButtonClick = () => {
+		if (!isValueValid) return;
+		const updatedList = [...list, { id: Date.now(), value }];
+		setList(updatedList);
+		setValue('');
+		setError('');
+
+	}
+
+	const isValueValid = value.length >= 3;
+
 	return (
 		<div className={styles.app}>
 			<h1 className={styles['page-heading']}>Ввод значения</h1>
 			<p className={styles['no-margin-text']}>
 				Текущее значение <code>value</code>: "<output
 					className={styles['current-value']}
-				></output>"
+				>{value}</output>"
 			</p>
-			<div className={styles.error}>
-				Введенное значение должно содержать минимум 3 символа
-			</div>
+			{error !== '' && <div className={styles.error}>{error}</div>}
 			<div className={styles['buttons-container']}>
-				<button className={styles.button}>Ввести новое</button>
-				<button className={styles.button} disabled>Добавить в список</button>
+				<button className={styles.button} onClick={onInputButtonClick}>Ввести новое</button>
+				<button className={styles.button} onClick={onAddButtonClick} disabled={!isValueValid}>Добавить в список</button>
 			</div>
 			<div className={styles['list-container']}>
 				<h2 className={styles['list-heading']}>Список:</h2>
-				<p className={styles['no-margin-text']}>Нет добавленных элементов</p>
-				<ul className={styles.list}>
-					<li className={styles['list-item']}>Первый элемент</li>
-				</ul>
+				{list.length > 0 ? (
+					<ul className={styles.list}>
+						{list.map((item) => (
+							<li className={styles['list-item']} key={item.id}>
+								{item.value}
+							</li>
+						))}
+					</ul>
+				) : (
+					<p className={styles['no-margin-text']}>Нет добавленных элементов</p>
+				)}
 			</div>
 		</div>
 	);
